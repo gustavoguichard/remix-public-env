7927d81b9f65f4bf44cc0a8759aed0f8f0f7e1f3
Guga Guichard <gustavoguichard@gmail.com>
2024-06-13 16:13:48 -0300

    Remove lodash's pick in favor of a custom strongly typed one

--- app/environment.server.tsx
+++ app/environment.server.tsx
@@ -1,5 +1,12 @@
-import pick from 'lodash/pick'
 import * as z from 'zod'
+
+function typedPick<T extends {}, U extends Array<keyof T>>(obj: T, keys: U) {
+  let result = {} as Pick<T, U[number]>
+  for (const key of keys) {
+    result[key] = obj[key]
+  }
+  return result
+}
 
 const environmentSchema = z.object({
   NODE_ENV: z
@@ -15,7 +22,7 @@ const environment = () => environmentSchema.parse(process.env)
 
 function getPublicKeys() {
   return {
-    publicKeys: pick(environment(), [
+    publicKeys: typedPick(environment(), [
       'STRIPE_PUBLIC_KEY',
       'GOOGLE_MAPS_API_KEY',
     ]),
