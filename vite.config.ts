7f8afb0b877b1f1f16441b97236af062f146b5ab
Guga Guichard <gustavoguichard@gmail.com>
2024-06-13 17:37:40 -0300

    Update dependencies

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { vitePlugin as remix } from '@remix-run/dev'
+import { installGlobals } from '@remix-run/node'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+installGlobals()
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [remix(), tsconfigPaths()],
+  }
+})
